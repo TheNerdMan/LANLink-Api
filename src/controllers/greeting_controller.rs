c22210361e0edcaf45b3043904c8ac510efa8591
TheNerdMan <alex.dave999@gmail.com>
2024-10-11 21:19:31 +0100

    🐛 Fixed gets in greeting_controller.rs

--- src/controllers/greeting_controller.rs
+++ src/controllers/greeting_controller.rs
@@ -2,13 +2,14 @@ use axum::{response::Html, routing::get, Router};
 
 pub fn router() -> Router {
     Router::new()
-        .route("/api/v1/greeting/hello", get(hello()))
-        .route("/api/v1/greeting/good-bye", get(good_bye()))
+        .route("/api/v1/greeting/hello", get(hello))
+        .route("/api/v1/greeting/good-bye", get(good_bye))
 }
 
 async fn hello() -> Html<&'static str> {
     Html("<h1>Hello, World! MOTHER FUCKKERRRR</h1>")
 }
+
 async fn good_bye() -> Html<&'static str> {
     Html("<h1>👋 Good bye!</h1>")
 }
