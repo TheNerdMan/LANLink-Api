824f99600d2d2e72aef57b69a759ae12492347bd
TheNerdMan <alex.dave999@gmail.com>
2024-10-15 16:38:08 +0100

    🐛 Use update/create structs to make sure id/public-id is not written in places

--- src/features/user/models/user_model.rs
+++ src/features/user/models/user_model.rs
@@ -2,12 +2,36 @@ use diesel::prelude::*;
 use serde::Serialize;
 use uuid::Uuid;
 
-#[derive(Queryable, Selectable, Serialize, Insertable, AsChangeset)]
+#[derive(Queryable, Selectable, Serialize)]
 #[diesel(table_name = crate::schema::users)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct UserModel{
     pub id: i32,
     pub publicid: Uuid,
+    pub username: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub discord_username: String,
+    pub steam_url: String,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = crate::schema::users)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct NewUserModel{
+    pub publicid: Uuid,
+    pub username: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub discord_username: String,
+    pub steam_url: String,
+}
+
+#[derive(AsChangeset)]
+#[diesel(table_name = crate::schema::users)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct UpdateUserModel{
+    pub id: i32,
     pub username: String,
     pub first_name: String,
     pub last_name: String,
@@ -40,26 +64,26 @@ impl UserModel{
         }
     }
 
-    pub fn create_new_user_for_db(&self) -> UserModel {
-        let mut new_user = UserModel::new();
-        new_user.publicid = Uuid::new_v4();
-        new_user.username = self.username.clone();
-        new_user.first_name = self.first_name.clone();
-        new_user.last_name = self.last_name.clone();
-        new_user.discord_username = self.discord_username.clone();
-        new_user.steam_url = self.steam_url.clone();
-        new_user
+    pub fn create_new_user_for_db(&self) -> NewUserModel {
+        NewUserModel{
+            publicid: Uuid::new_v4(),
+            username: self.username.clone(),
+            first_name: self.first_name.clone(),
+            last_name: self.last_name.clone(),
+            discord_username: self.discord_username.clone(),
+            steam_url: self.steam_url.clone()
+        }
     }
     
-    pub fn create_update_user_for_db(&self) -> UserModel {
+    pub fn create_update_user_for_db(&self) -> UpdateUserModel {
         // we are making sure not to delete the user public ID as it should be static
-        let mut update_user = UserModel::new();
-        update_user.id = self.id.clone();
-        update_user.username = self.username.clone();
-        update_user.first_name = self.first_name.clone();
-        update_user.last_name = self.last_name.clone();
-        update_user.discord_username = self.discord_username.clone();
-        update_user.steam_url = self.steam_url.clone();
-        update_user
+        UpdateUserModel{
+            id: self.id.clone(),
+            username: self.username.clone(),
+            first_name: self.first_name.clone(),
+            last_name: self.last_name.clone(),
+            discord_username: self.discord_username.clone(),
+            steam_url: self.steam_url.clone(),
+        }
     }
 }
