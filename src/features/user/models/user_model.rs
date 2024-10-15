b902bbd4308f6baf8641984f9a9d5f65543e5970
TheNerdMan <alex.dave999@gmail.com>
2024-10-15 14:54:28 +0100

    ✨ Added User repo

--- src/features/user/models/user_model.rs
+++ src/features/user/models/user_model.rs
@@ -2,7 +2,7 @@ use diesel::prelude::*;
 use serde::Serialize;
 use uuid::Uuid;
 
-#[derive(Queryable, Selectable, Serialize)]
+#[derive(Queryable, Selectable, Serialize, Insertable, AsChangeset)]
 #[diesel(table_name = crate::schema::users)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct UserModel{
---
+++ src/features/user/repos/user_repo.rs
@@ -0,0 +1,166 @@
+use deadpool_diesel::postgres::Pool;
+use diesel::prelude::*;
+use diesel::associations::HasTable;
+use diesel::{QueryDsl, SelectableHelper};
+use uuid::Uuid;
+
+// internal uses
+use crate::core::db_connection::db_connection::create_connection;
+use crate::core::errors::error::AppError;
+use crate::features::user::models::user_model::UserModel;
+use crate::schema::users::dsl::*;
+
+pub async fn get_all_users(
+    pool: Pool,
+) -> Option<Vec<UserModel>> {
+    let conn = match create_connection(pool).await {
+        Some(conn) => conn,
+        None => return None,
+    };
+
+    let result = conn.interact(|c| {
+        users
+            .select(UserModel::as_select())
+            .load::<UserModel>(c)
+    })
+        .await
+        .map_err(|e| AppError::DatabaseQueryError(e.to_string()));
+
+    match result {
+        Ok(equipment) => {
+            match equipment {
+                Ok(equipment) => Some(equipment),
+                Err(_) => None
+            }
+        },
+        Err(_) => None
+    }
+}
+
+pub async fn get_user_by_id(
+    pool: Pool,
+    request_id: i32,
+) -> Option<UserModel> {
+    let conn = match create_connection(pool).await {
+        Some(conn) => conn,
+        None => return None,
+    };
+
+    let result = conn.interact(move |c| {
+        users::table()
+            .find(request_id)
+            .select(UserModel::as_select())
+            .first(c)
+    })
+        .await
+        .map_err(|e| AppError::DatabaseQueryError(e.to_string()));
+
+    match result {
+        Ok(equipment) => {
+            match equipment {
+                Ok(equipment) => Some(equipment),
+                Err(_) => None
+            }
+        },
+        Err(_) => None
+    }
+}
+
+pub async fn get_user_by_public_id(
+    pool: Pool,
+    public_id: Uuid,
+) -> Option<UserModel> {
+    let conn = match create_connection(pool).await {
+        Some(conn) => conn,
+        None => return None,
+    };
+
+    let result = conn.interact(move |c| {
+        users::table()
+            .filter(publicid.eq(public_id))
+            .select(UserModel::as_select())
+            .first(c)
+    })
+        .await
+        .map_err(|e| AppError::DatabaseQueryError(e.to_string()));
+
+    match result {
+        Ok(equipment) => {
+            match equipment {
+                Ok(equipment) => Some(equipment),
+                Err(_) => None
+            }
+        },
+        Err(_) => None
+    }
+}
+
+pub async fn create_or_update_user(
+    pool: Pool,
+    user_model: UserModel,
+)  {
+    if user_model.id == 0 {
+        create_user(pool, user_model).await;
+    } else {
+        update_user(pool, user_model).await;
+    }
+}
+
+async fn create_user(
+    pool: Pool,
+    user_model: UserModel,
+) -> Option<UserModel> {
+    let conn = match create_connection(pool).await {
+        Some(conn) => conn,
+        None => return None,
+    };
+
+    let result = conn.interact(move |c| {
+        diesel::insert_into(users::table())
+            .values(&user_model)
+            .returning(UserModel::as_returning())
+            .get_result(c)
+    })
+        .await
+        .map_err(|e| AppError::DatabaseQueryError(e.to_string()));
+
+    match result {
+        Ok(equipment) => {
+            match equipment {
+                Ok(equipment) => Some(equipment),
+                Err(_) => None
+            }
+        },
+        Err(_) => None
+    }
+}
+
+async fn update_user(
+    pool: Pool,
+    user_model: UserModel,
+) -> Option<UserModel> {
+    let conn = match create_connection(pool).await {
+        Some(conn) => conn,
+        None => return None,
+    };
+
+    let result = conn.interact(move |c| {
+        diesel::update(users::table())
+            .filter(id.eq(user_model.id))
+            .set(&user_model)
+            .returning(UserModel::as_returning())
+            .get_result(c)
+    })
+        .await
+        .map_err(|e| AppError::DatabaseQueryError(e.to_string()));
+
+    match result {
+        Ok(equipment) => {
+            match equipment {
+                Ok(equipment) => Some(equipment),
+                Err(_) => None
+            }
+        },
+        Err(_) => None
+    }
+}
